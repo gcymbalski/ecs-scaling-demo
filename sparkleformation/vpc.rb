a7e53ec8e692c71650c830f196a332c28c564044
Greg Cymbalski <cymbalski@gmail.com>
2017-03-07 15:36:30 -0800

    quick rubocop cleanup

--- sparkleformation/vpc.rb
+++ sparkleformation/vpc.rb
@@ -1,3 +1,2 @@
-SparkleFormation.new(:cluster_vpc, :inherit => :lazy_vpc__nat_subnet_vpc).load(:ecs).overrides do
-
+SparkleFormation.new(:cluster_vpc, inherit: :lazy_vpc__nat_subnet_vpc).load(:ecs).overrides do
 end
